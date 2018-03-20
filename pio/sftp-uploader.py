597498337a8e46494a9b4298c1ca92951539e36d
LucFabresse <luc.fabresse@gmail.com>
2018-03-20 04:09:43 +0100

    setup scp upload to node-red OTA server

--- pio/sftp-uploader.py
+++ pio/sftp-uploader.py
@@ -3,4 +3,4 @@ from base64 import b64decode
 
 env.Replace(UPLOADER="scp")
 env.Replace(UPLOADERFLAGS="")
-env.Replace(UPLOADCMD="$UPLOADER $SOURCES " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + "/" + b64decode(ARGUMENTS.get("PIOENV")) + ".bin")
+env.Replace(UPLOADCMD="$UPLOADER $SOURCES " + b64decode(ARGUMENTS.get("UPLOAD_PORT")))
