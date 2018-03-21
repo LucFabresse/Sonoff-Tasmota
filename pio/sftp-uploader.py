2d8b8b837848200c23877e1c65c8a4d9b984b973
LucFabresse <luc.fabresse@gmail.com>
2018-03-21 20:57:49 +0100

    improve OTA support with scp

--- pio/sftp-uploader.py
+++ pio/sftp-uploader.py
@@ -1,6 +1,11 @@
 Import("env")
 from base64 import b64decode
+# from pprint import pprint
 
 env.Replace(UPLOADER="scp")
 env.Replace(UPLOADERFLAGS="")
-env.Replace(UPLOADCMD="$UPLOADER $SOURCES " + b64decode(ARGUMENTS.get("UPLOAD_PORT")))
+# env.Replace(UPLOADCMD="$UPLOADER $SOURCES " + b64decode(ARGUMENTS.get("UPLOAD_PORT")))
+OTAFirmwareName = b64decode(ARGUMENTS.get("PIOENV")) + ".bin"
+OTAScpFullPath = b64decode(ARGUMENTS.get("UPLOAD_PORT")) + "/" + OTAFirmwareName
+OTAUrl = b64decode(ARGUMENTS.get("OTA_URL_PREFIX")) + "/" + OTAFirmwareName
+env.Replace(UPLOADCMD="$UPLOADER $SOURCES " + OTAScpFullPath + "&& echo To " + OTAScpFullPath + "&& echo "+OTAUrl)
